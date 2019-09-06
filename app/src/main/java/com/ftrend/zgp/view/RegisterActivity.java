1a3d62cd41d635c870efa0863a397e517aef4b9a
LZQ <liziqiang@ftrend.cn>
2019-09-06 09:42:29 +0800

    2019年9月6日09:42:09 1.补交图片。

---
+++ app/src/main/java/com/ftrend/zgp/view/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.ftrend.zgp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ftrend.zgp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
