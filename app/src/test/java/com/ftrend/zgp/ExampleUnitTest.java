077d82f8e1aab4163a030d5e9be1a1e3c45bce57
liuhongbin <liuhongbin@ftrend.cn>
2019-09-07 15:09:07 +0800

    增加2个测试用户密码

--- app/src/test/java/com/ftrend/zgp/ExampleUnitTest.java
+++ app/src/test/java/com/ftrend/zgp/ExampleUnitTest.java
@@ -25,10 +25,12 @@ public class ExampleUnitTest {
         System.out.println("解密测试: " + encrypted + " => " + EncryptUtill.pwdDecrypt(encrypted));
         System.out.println();
         System.out.println("用户密码: 李雪勤 => " + EncryptUtill.pwdDecrypt("6=R>D<H?."));
+        System.out.println("用户密码: 蒋丽 => " + EncryptUtill.pwdDecrypt("79>>H*H0Z"));
         System.out.println("用户密码: 高丽 => " + EncryptUtill.pwdDecrypt("71S><CH;."));
         System.out.println("用户密码: 授权卡号 => " + EncryptUtill.pwdDecrypt("6A<>0/I;4"));
         System.out.println("用户密码: 退货账号 => " + EncryptUtill.pwdDecrypt("7ANAL?;?)"));
         System.out.println("用户密码: 宋昆林 => " + EncryptUtill.pwdDecrypt("7C?A>7"));
+        System.out.println("用户密码: 周显云 => " + EncryptUtill.pwdDecrypt("7-:>D,H;5"));
 
 //        System.out.println(Charset.availableCharsets());
     }
