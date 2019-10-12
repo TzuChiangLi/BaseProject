02eee1be6aebd13295147f0855c25196ffdc4524
liuhongbin <liuhongbin@ftrend.cn>
2019-10-12 08:20:22 +0800

    增加收钱吧接口部分工具类

---
+++ app/src/main/java/com/ftrend/zgp/utils/pay/SqbPayHelper.java
@@ -0,0 +1,36 @@
+package com.ftrend.zgp.utils.pay;
+
+/**
+ * 收钱吧API接口功能类
+ * Copyright (C),青岛致远方象软件科技有限公司
+ *
+ * @author liuhongbin@ftrend.cn
+ * @since 2019/10/11
+ */
+public class SqbPayHelper {
+    /**
+     * 收钱吧接入域名
+     */
+    public static final String apiDomain = "https://api.shouqianba.com";
+    /**
+     * 服务商ID
+     */
+    public static final String vendorSn = "20160407103537";
+    /**
+     * 服务商Key
+     */
+    public static final String vendorKey = "a8bbaa0aeb3daf40f5924a3a9b694d00";
+    /**
+     * 服务商AppId
+     */
+    public static String appId = "";
+
+    /**
+     * 终端号
+     */
+    public static String terminalSn = "";
+    /**
+     * 终端密钥
+     */
+    public static String terminalKey = "";
+}
